16fe4b66e5e71539c63c5ee280fb5b944551770d
Steve Bilogan <steve.bilogan@gmail.com>
2019-10-19 11:44:19 -0400

    Add UI and initial VM plumbing

---
+++ Shine.Uno.Shared/ViewModels/WeatherViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Input;
+
+namespace Shine.Uno.Shared.ViewModels
+{
+	public class WeatherViewModel
+	{
+		public ICommand GetWeatherCommand;
+
+		public WeatherViewModel()
+		{
+			
+		}
+	}
+}
